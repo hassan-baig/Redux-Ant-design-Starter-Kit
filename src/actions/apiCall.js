e7435958c31a52d3beb726e899b900f7897e9998
Hassan Baig <hassanbaigdev@gmail.com>
2019-12-11 20:02:31 +0500

    Starter Kit

---
+++ src/actions/apiCall.js
@@ -0,0 +1,12 @@
+import { SOMETHING } from "./types";
+
+export const fetchPosts = () => dispatch => {
+  //   fetch('https://jsonplaceholder.typicode.com/posts')
+  //     .then(res => res.json())
+  //     .then(posts =>
+  //       dispatch({
+  //         type: FETCH_POSTS,
+  //         payload: posts
+  //       })
+  //     );
+};
